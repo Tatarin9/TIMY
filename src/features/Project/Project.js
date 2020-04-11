323e024f5f49794854d937923e9bbf548d54ef04
yfooks <yfooks@gmail.com>
2020-04-11 23:47:01 +0300

    add form with generic input and validation

--- src/features/Project/Project.js
+++ src/features/Project/Project.js
@@ -7,19 +7,37 @@ import Button from '@material-ui/core/Button';
 import classes from './Project.css';
 import Aux from '../../shared/hoc/Aux/Aux';
 import withErrorHandler from '../../shared/hoc/withErrorHandler/withErrorHandler';
+import {setFormControl, checkFromElementValidity} from '../../shared/Helpers';
+import Input from '../../shared/UI/Input/Input';
 
 class Project extends Component {
 
     state = {
-        name: '',
-        customer: '',
-        dueDate: '',
-        budget: '',
-        owner: '',
+        projectForm: {
+            controls: [
+                setFormControl('projectNameId', 'projectName', 'input', 'text', 'Project name', '', { required: true}, false, false),
+                setFormControl('projectCustomerId','customer', 'input', 'text', 'Customer name', '', { required: true}, false, false),
+                setFormControl('projectCustomerIdId','customerId', 'input', 'text', 'Customer id', '', { required: true}, false, false),
+            ],
+            valid: true
+        },
         loading: false,
         submitted: false,
         error: null,
     };
+
+    // projectName: '',
+    // customer: '',
+    // customerId: '',
+    // approvalDate: '',
+    // dueDate: '',
+    // budgetHours: '',
+    // actualHours: '',
+    // ownerId: '',
+    // ownerName: '',
+    // steps: [],
+
+
 
     componentDidMount() {
         const projectId = this.props.match.params.id;
@@ -41,11 +59,37 @@ class Project extends Component {
         console.log('load data from backend by project id');
     }
 
-    submitProjectHandler = () => {
+    inputChangedHandler = (event, inputIdentifier) => {
+        debugger
+        const updatedProjectForm = {
+            ...this.state.projectForm
+        };
+        const controlIndex = updatedProjectForm.controls.findIndex( (control) => control.id === inputIdentifier );
+        const updatedFormElement = {
+            ...updatedProjectForm.controls[controlIndex]
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = checkFromElementValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
+        updatedProjectForm.controls[controlIndex] = updatedFormElement;
+
+        let isFormValid = true;
+        for (let inputIdentifier in updatedProjectForm) {
+            isFormValid = updatedProjectForm.controls[controlIndex].valid && isFormValid;
+        }
+        updatedProjectForm.valid = isFormValid;
+        this.setState({projectForm: updatedProjectForm});
+    }
+
+    submitProjectHandler = (event) => {
+        event.preventDefault();
+
         const project = {
             name: 'stam',
             action: 'clicked',
-            changedByUserId: 123
+            changedByUserId: 123,
+            status: 'completed'
         };
 
         axios.post('/demo/projects.json', project)
@@ -61,15 +105,39 @@ class Project extends Component {
     }
 
     render() {
+        let form = (
+            <form onSubmit={this.submitProjectHandler}>
+                {this.state.projectForm.controls.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.elementType}
+                        elementConfig={formElement.elementConfig}
+                        value={formElement.value}
+                        invalid={!formElement.valid}
+                        shouldValidate={formElement.validation}
+                        touched={formElement.touched}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
+            </form>
+        );
+        // if ( this.state.loading ) {
+        //     form = <Spinner />;
+        // }
+
+
         let project = null;
         project = (
             <div className={classes.Project}>
                 <h4>Project data</h4>
                 <p>Project id is {this.props.match.params.id}</p>
                 <form>
+                    <TextField id="outlined-basic" label="Project name" variant="outlined" size="small"/>
                     <TextField id="outlined-basic" label="Customer" variant="outlined" size="small"/>
                     <TextField id="outlined-basic" label="Due date" variant="outlined" size="small"/>
+                    <TextField id="outlined-basic" label="Owner" variant="outlined" size="small"/>
                 </form>
+                {form}
                 <Button variant="contained" color="primary"
                         onClick={this.submitProjectHandler}>Submit</Button>
             </div>
---
+++ src/shared/Helpers.js
@@ -0,0 +1,49 @@
+export function setFormControl(id, name, elementType, configType, placeholder, value, validation, valid, touched) {
+    const control = {
+        id: id,
+        name: name,
+        elementType: elementType,
+        elementConfig: {
+            type: configType,
+            placeholder: placeholder
+        },
+        value: value,
+        validation: {
+            ...validation
+        },
+        valid: valid,
+        touched: touched
+    }
+    return control;
+}
+
+export function checkFromElementValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+        return true;
+    }
+
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid
+    }
+
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid
+    }
+
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    return isValid;
+}
